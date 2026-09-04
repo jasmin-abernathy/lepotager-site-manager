28cd5a6dc98167bc31b1c48d9dd9411b63eae6e5
Jasmin-potager <jasmin@lepotager.org>
2026-09-04 17:24:24 +0200

    Fix reusable centered card scope typing

---
+++ app/src/main/java/org/lepotager/sitemanager/ui/ColumnAlias.kt
@@ -0,0 +1,7 @@
+package org.lepotager.sitemanager.ui
+
+/**
+ * Classifier alias used by Screens.kt for a composable Column receiver. Compose's
+ * `Column(...)` function lives in the callable namespace, so both names coexist.
+ */
+typealias Column = androidx.compose.foundation.layout.ColumnScope
