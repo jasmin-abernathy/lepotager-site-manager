e2c9a6700951226316c750104765798b8efab580
Jasmin-potager <jasmin@lepotager.org>
2026-09-22 10:10:37 +0200

    Add SwiftUI iOS host and simulator app build

---
+++ iosApp/iosApp/ContentView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+import UIKit
+import MonManagerShared
+
+struct ComposeView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        IosManagerRuntimeKt.MainViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+}
+
+struct ContentView: View {
+    var body: some View {
+        ComposeView()
+            .ignoresSafeArea(.all, edges: .bottom)
+    }
+}
