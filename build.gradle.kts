60162d21789ac789c2835c775661a1a939a2344f
Jasmin-potager <jasmin@lepotager.org>
2026-09-22 09:44:54 +0200

    Align Compose Multiplatform with Kotlin 2.2 iOS support

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.android.library") version "8.10.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
     id("org.jetbrains.kotlin.multiplatform") version "2.2.21" apply false
-    id("org.jetbrains.compose") version "1.11.1" apply false
+    id("org.jetbrains.compose") version "1.9.3" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21" apply false
     id("com.google.devtools.ksp") version "2.2.21-2.0.5" apply false
